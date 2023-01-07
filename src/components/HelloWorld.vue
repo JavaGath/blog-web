484aacd5f0524462f8c6f5afaeff8aec435c0b30
JavaGath <i.izrailtenko@web.de>
2023-01-07 18:21:16 +0100

    init vue ts project

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -50,6 +50,14 @@
           >unit-jest</a
         >
       </li>
+      <li>
+        <a
+          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript"
+          target="_blank"
+          rel="noopener"
+          >typescript</a
+        >
+      </li>
     </ul>
     <h3>Essential Links</h3>
     <ul>
@@ -110,13 +118,17 @@
   </div>
 </template>
 
-<script>
-export default {
-  name: "HelloWorld",
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({
   props: {
     msg: String,
   },
-};
+})
+export default class HelloWorld extends Vue {
+  msg!: string;
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
