6c4486622efe9252b783a4e3706912e366c39104
JavaGath <i.izrailtenko@web.de>
2023-01-08 20:22:10 +0100

    delete until empty sources

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="private">
+    <h1>This is some page with a private content!</h1>
   </div>
 </template>
 
 <style>
 @media (min-width: 1024px) {
-  .about {
+  .private {
     min-height: 100vh;
     display: flex;
     align-items: center;
