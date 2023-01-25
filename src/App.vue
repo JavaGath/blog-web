da997887932ac62a555ae84f8be25b61b831dbf8
JavaGath <i.izrailtenko@web.de>
2023-01-25 21:17:01 +0100

    css for footer

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,11 @@ import HeaderComponent from "@/components/HeaderComponent.vue";
 </script>
 
 <template>
-  <HeaderComponent></HeaderComponent>
-  <RouterView></RouterView>
+  <div class="container-wrapper">
+    <HeaderComponent></HeaderComponent>
+    <RouterView class="container-content"></RouterView>
+    <footer>
+      <h2>Contact info</h2>
+    </footer>
+  </div>
 </template>
