323fbd58ecbd5a806f1d659f694c36c56622f898
JavaGath <i.izrailtenko@web.de>
2023-02-02 20:01:10 +0100

    refactoring

--- src/components/HeaderLogo.vue
+++ src/components/HeaderLogo.vue
@@ -1,10 +1,8 @@
 <template>
-  <div class="">
+  <div>
     <p>
       <img class="h-20" alt="JavaGath logo" src="../assets/logo.png" />
     </p>
     <p class="pl-3">| JavaGath</p>
   </div>
 </template>
-
-<script setup lang="ts"></script>
