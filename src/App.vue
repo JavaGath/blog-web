21599ab75a36bac1f9a82739c47c63982cd38f98
JavaGath <i.izrailtenko@web.de>
2023-02-02 21:38:14 +0100

    css refactoring

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,10 @@
 <template>
   <div class="flex flex-col h-screen justify-between">
-    <HeaderComponent
-      class="h-20 cursor-default bg-logo-color select-none text-3xl font-bold text-slate-90 grid grid-cols-12 text-blue-50"
-    ></HeaderComponent>
+    <HeaderComponent class="h-20 bar"></HeaderComponent>
     <RouterView
       class="mb-auto h-full grid grid-rows-2 grid-cols-2"
     ></RouterView>
-    <FooterComponent class="h-52 bg-blue-500"></FooterComponent>
+    <FooterComponent class="h-52 bar"></FooterComponent>
   </div>
 </template>
 
