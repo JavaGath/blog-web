87629e483e0474293e63d02707e46aaaceb37bea
JavaGath <i.izrailtenko@web.de>
2023-02-16 22:07:51 +0100

    footer contact on tailwind css

--- src/components/FooterContact.vue
+++ src/components/FooterContact.vue
@@ -1,21 +1,29 @@
 <template>
   <div>
-    <h2 class="no-select default-cursor">Contact info</h2>
-    <p class="empty-line-space no-select default-cursor">
-      If you have any questions, thoughts or ideas (e.g. suggestions for future
-      articles) contact me via email.
-    </p>
-    <div class="row-wrapper">
-      <i class="fa fa-envelope-o color-orange" aria-hidden="true"></i>
-      <p class="one-row">ievgenii.izrailtenko@gmail.com</p>
-    </div>
-    <div class="row-wrapper">
-      <i class="fa fa-map-marker color-orange" aria-hidden="true"></i>
-      <p class="one-row">Düsseldorf, Germany</p>
+    <h2>Contact info</h2>
+    <div class="text-sm">
+      <p class="mb-4 text-blue-50">
+        If you have any questions, thoughts or ideas (e.g. suggestions for
+        future articles) contact me via email.
+      </p>
+      <div>
+        <i
+          class="fa fa-envelope-o text-button-color w-1/20"
+          aria-hidden="true"
+        ></i>
+        <p class="inline text-blue-50 select-text m-0 p-0 text-left w-19/20">
+          ievgenii.izrailtenko@gmail.com
+        </p>
+      </div>
+      <div>
+        <i
+          class="fa fa-map-marker text-button-color w-1/20"
+          aria-hidden="true"
+        ></i>
+        <p class="inline text-blue-50 m-0 p-0 text-left w-19/20">
+          Düsseldorf, Germany
+        </p>
+      </div>
     </div>
   </div>
 </template>
-
-<script setup lang="ts"></script>
-
-<style scoped></style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,10 @@ module.exports = {
         'logo-color': '#1E1E1C',
         'button-color': '#DA5408',
       },
+      width: {
+        '1/20': '5%',
+        '19/20': '95%',
+      },
     },
   },
   plugins: [],
