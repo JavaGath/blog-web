bf2cd6a072577e01930c9f0423bc4129d86b19e6
JavaGath <i.izrailtenko@web.de>
2023-02-16 22:06:43 +0100

    footer new colomn spacing

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,11 +1,11 @@
 <template>
   <footer>
-    <FooterContact class="col-start-1 col-end-5 pt-2 pl-2"></FooterContact>
+    <FooterContact class="col-start-2 col-end-5 pt-2 pl-2"></FooterContact>
     <FooterProjects
-      class="col-start-5 col-end-9 pt-2 pl-2 no-select"
+      class="col-start-5 col-end-9 pt-2 pl-2 no-select bg-blue-600"
     ></FooterProjects>
     <FooterSocial
-      class="col-start-9 col-end-13 pt-2 pl-2 no-select"
+      class="col-start-9 col-end-12 pt-2 pl-2 no-select bg-red-600"
     ></FooterSocial>
   </footer>
 </template>
@@ -15,5 +15,3 @@ import FooterContact from '@/components/FooterContact.vue'
 import FooterProjects from '@/components/FooterProjects.vue'
 import FooterSocial from '@/components/FooterSocial.vue'
 </script>
-
-<style scoped></style>
