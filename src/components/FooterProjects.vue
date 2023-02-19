1e7fca7bb7b923fd1d4d68eec371cd87f5f47241
JavaGath <i.izrailtenko@web.de>
2023-02-20 00:04:46 +0100

    footer social

--- src/components/FooterProjects.vue
+++ src/components/FooterProjects.vue
@@ -1,17 +1,27 @@
 <template>
   <div>
-    <h2 class="default-cursor">Project repositories</h2>
+    <h2>Project repositories</h2>
     <div class="flex text-sm">
       <div class="w-1/2 pl-5">
-        <a href="https://github.com/JavaGath/blog-web">Blog Web</a>
-        <a href="https://github.com/JavaGath/blog-api">Blog Api</a>
-        <a href="https://github.com/JavaGath/authenticator-api"
+        <a class="list-item" href="https://github.com/JavaGath/blog-web"
+          >Blog Web</a
+        >
+        <a class="list-item" href="https://github.com/JavaGath/blog-api"
+          >Blog Api</a
+        >
+        <a
+          class="list-item"
+          href="https://github.com/JavaGath/authenticator-api"
           >Authenticator</a
         >
       </div>
       <div class="w-1/2 pl-5">
-        <a href="https://github.com/JavaGath/util-web">Util Web</a>
-        <a href="https://github.com/JavaGath/orchestrator-api">Orechestrator</a>
+        <a class="list-item" href="https://github.com/JavaGath/util-web"
+          >Util Web</a
+        >
+        <a class="list-item" href="https://github.com/JavaGath/orchestrator-api"
+          >Orechestrator</a
+        >
       </div>
     </div>
   </div>
--- src/components/FooterSocial.vue
+++ src/components/FooterSocial.vue
@@ -1,42 +1,35 @@
 <template>
-  <div class="no-select">
+  <div>
     <h2 class="default-cursor">Social Media</h2>
-    <a href="https://github.com/JavaGath">
-      <img
-        class="icon"
-        alt="Github icon"
-        src="../components/icons/github.png"
-      />
-    </a>
-    <a href="https://www.youtube.com/@javagath">
-      <img
-        class="icon"
-        alt="Youtube icon"
-        src="../components/icons/youtube.png"
-      />
-    </a>
-    <a href="https://t.me/javagath">
-      <img
-        class="icon"
-        alt="Telegram icon"
-        src="../components/icons/telegram.png"
-      />
-    </a>
-    <a href="https://www.reddit.com/user/JavaGath">
-      <img
-        class="icon"
-        alt="Reddit icon"
-        src="../components/icons/reddit.png"
-      />
-    </a>
+    <div class="inline-flex">
+      <a href="https://github.com/JavaGath">
+        <img
+          class="icon"
+          alt="Github icon"
+          src="../components/icons/github.png"
+        />
+      </a>
+      <a href="https://www.youtube.com/@javagath">
+        <img
+          class="icon"
+          alt="Youtube icon"
+          src="../components/icons/youtube.png"
+        />
+      </a>
+      <a href="https://t.me/javagath">
+        <img
+          class="icon"
+          alt="Telegram icon"
+          src="../components/icons/telegram.png"
+        />
+      </a>
+      <a href="https://www.reddit.com/user/JavaGath">
+        <img
+          class="icon"
+          alt="Reddit icon"
+          src="../components/icons/reddit.png"
+        />
+      </a>
+    </div>
   </div>
 </template>
-
-<script setup lang="ts"></script>
-
-<style scoped>
-.icon {
-  width: 45px;
-  padding: 2px;
-}
-</style>
